95459aa6246f7504243764dcf90beba2bc8aa15d
tetraphoz <tetraphosphorus@gmail.com>
2026-09-13 21:47:14 -0600

    default libvirt clients to system connection

--- hosts/p52/containers.nix
+++ hosts/p52/containers.nix
@@ -5,6 +5,11 @@
 
   virtualisation.libvirtd.enable = true;
 
+  # The default libvirt NAT network belongs to the system daemon.  Without
+  # this, virsh and virt-manager may connect to qemu:///session instead,
+  # where the system's `default` network does not exist.
+  environment.variables.LIBVIRT_DEFAULT_URI = "qemu:///system";
+
   programs.virt-manager.enable = true;
 
   environment.systemPackages = with pkgs; [
